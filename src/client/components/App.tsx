f0fcbf8bc43cc5c785bc673615b57ea147da6e2e
jbrasko2 <jbrasko0508@gmail.com>
2026-05-06 22:56:38 -0500

    style: add tailwind classes to App.tsx

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,11 +1,11 @@
 import type { FunctionComponent } from 'react';
 
 const App: FunctionComponent = () => {
-    return (
-        <div>
-            <h1>dwellr</h1>
-        </div>
-    );
+  return (
+      <div className='p-8'>
+          <h1 className='text-3xl font-bold'>dwellr</h1>
+      </div>
+  );
 };
 
 export default App;
