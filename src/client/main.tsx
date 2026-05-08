8c4e3a5446632c3a366ea1779e7853a520ca7884
jbrasko2 <jbrasko0508@gmail.com>
2026-05-07 23:04:14 -0500

    feat: use providers in main.tsx

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import '@/styles/globals.css';
+import App from '@/components/App';
+import { RouteProvider } from '@/providers/router-provider';
+import { ThemeProvider } from '@/providers/theme-provider';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <RouteProvider>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </RouteProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
+);
