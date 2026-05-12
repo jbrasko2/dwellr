e036459ab956af88e28a809f81ac069b1f1fd911
jbrasko2 <jbrasko0508@gmail.com>
2026-05-11 23:35:28 -0500

    feat: set up vitest

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
@@ -9,5 +9,10 @@ export default defineConfig({
         alias: {
             '@': resolve(__dirname, '.'),
         },
+    },
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: ['./vitest.setup.ts'],
     },
 });
