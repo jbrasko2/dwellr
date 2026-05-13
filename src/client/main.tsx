824d327048b221728e0ff4a5c13b2d75467d80d9
jbrasko2 <jbrasko0508@gmail.com>
2026-05-13 11:36:02 -0500

    refactor: move App out of components folder

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import '@/styles/globals.css';
 import { RouteProvider } from '@/providers/router-provider';
 import { ThemeProvider } from '@/providers/theme-provider';
-import { App } from '@/components/App';
+import { App } from '@/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
