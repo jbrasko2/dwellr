02202ec24d0dab186628ca00b611d2d020c9a008
jbrasko2 <jbrasko0508@gmail.com>
2026-05-15 22:49:55 -0500

    feat: create formatters

---
+++ src/client/helpers/formatters.ts
@@ -0,0 +1,15 @@
+import type { ListingAddress } from '../../generated/types';
+
+export function formatPrice(price: number): string {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        maximumFractionDigits: 0,
+    }).format(price);
+}
+
+export function formatAddress(address: ListingAddress): string {
+    return [address.street, address.city, address.state, address.zip]
+        .filter(Boolean)
+        .join(', ');
+}
