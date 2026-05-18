25a08920a5b6528d84e5942f96e82badf24fd6dc
jbrasko2 <jbrasko0508@gmail.com>
2026-05-18 16:35:22 -0500

    feat: use mock search result for dev env

--- src/client/hooks/use-search.ts
+++ src/client/hooks/use-search.ts
@@ -1,6 +1,7 @@
 import { useLazyQuery } from '@apollo/client/react';
 import type { SearchResult } from '../../generated/types';
 import { SEARCH_LISTINGS } from '@/grapqhl/queries/search';
+import { mockSearchResult } from '@/pages/results/results.mock';
 
 export type SearchData = {
     search: SearchResult;
@@ -9,6 +10,15 @@ export type SearchData = {
 export const useSearch = () => {
     const [executeSearch, { loading, error, data }] =
         useLazyQuery<SearchData>(SEARCH_LISTINGS);
+
+    if (import.meta.env.VITE_USE_MOCK_DATA === 'true') {
+        return {
+            search: () => {},
+            loading: false,
+            error: undefined,
+            result: mockSearchResult,
+        };
+    }
 
     const search = (prompt: string) => {
         executeSearch({ variables: { prompt } });
