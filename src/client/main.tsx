24005b856c37322682527c67b83904003bd14fdc
jbrasko2 <jbrasko0508@gmail.com>
2026-05-17 21:14:31 -0500

    feat: set up ApolloProvider

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import '@/styles/globals.css';
 import { ThemeProvider } from '@/providers/theme-provider';
 import { App } from '@/App';
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({
+    link: new HttpLink({
+        uri: import.meta.env.VITE_GRAPHQL_URL ?? '/api/graphql',
+    }),
+    cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ApolloProvider client={client}>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ApolloProvider>
     </React.StrictMode>,
 );
