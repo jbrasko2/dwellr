dcaf96e1e1b883df954d28fc121423a8aaa2be39
jbrasko2 <jbrasko0508@gmail.com>
2026-05-17 21:16:12 -0500

    feat: create SEARCH_LISTINGS query

---
+++ src/client/grapqhl/queries/search.ts
@@ -0,0 +1,36 @@
+import { gql } from '@apollo/client';
+
+export const SEARCH_LISTINGS = gql`
+    query SearchListings($prompt: String!) {
+        search(prompt: $prompt) {
+            filters {
+                location
+                minBeds
+                maxBeds
+                minBaths
+                maxPrice
+                minPrice
+                propertyType
+                features
+            }
+            listings {
+                id
+                address {
+                    street
+                    city
+                    state
+                    zip
+                }
+                price
+                beds
+                baths
+                sqft
+                propertyType
+                imageUrl
+                listingUrl
+                description
+            }
+            total
+        }
+    }
+`;
