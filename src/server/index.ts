12924b00a86a6d254bf1d6ed4cc3fb384172b2e9
jbrasko2 <jbrasko0508@gmail.com>
2026-05-27 22:17:57 -0500

    feat: use cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,7 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@as-integrations/express5';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
+import cors from 'cors';
 import express from 'express';
 import { join } from 'path';
 import { searchResolvers } from './graphql/search/search.resolver';
@@ -10,6 +11,7 @@ import { searchResolvers } from './graphql/search/search.resolver';
 const app = express();
 const PORT = 4000;
 
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 const typesArray = loadFilesSync(join(__dirname, '**/*.graphql'));
